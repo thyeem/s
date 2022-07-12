2912a740f74785e42ace4ec52b9481d1f1351b77
Francis Lim <thyeem@gmail.com>
2022-07-12 09:31:11 +0900

    S?

--- src/S.hs
+++ src/S.hs
@@ -1,1 +1,11 @@
 module S where
+
+data SourceLoc = SourceLoc
+  { sourceName   :: FilePath
+  , sourceLine   :: !Int
+  , sourceColumn :: !Int
+  }
+  deriving (Show, Eq)
+
+initSourceLoc :: FilePath -> SourceLoc
+initSourceLoc n = SourceLoc n 1 1
